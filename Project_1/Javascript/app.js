ab0e55cba1bd55628e6e55b497d3add8609992db
Riyaz <riajmazumder711@gmail.com>
2022-08-13 23:10:39 +0600

    finish

---
+++ Project_1/Javascript/app.js
@@ -0,0 +1,7 @@
+const map = L.map('map').setView([23.761429, 90.429753], 5);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
+}).addTo(map),
+  L.marker([23.037027, 91.518711]).addTo(map).bindPopup('My location'),
+  hamburgerToggle();
